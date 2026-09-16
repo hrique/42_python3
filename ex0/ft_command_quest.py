e430264cb45d3fba596e0fd791c70d3dd691d570
Henrique Santana Frameschi <hesantan@c1r5p3.42sp.org.br>
2026-09-16 18:29:31 -0300

    ex0

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -9,14 +9,14 @@ def show_argv(arguments: list[str]) -> None:
     print(f"Program name: {arguments[0]}")
     if len(arguments) == 1:
         print("No arguments provided!")
-        print(f"{total} {len(arguments)}\n")
+        print(f"{total} {len(arguments)}")
         return
     else:
         args = arguments[1:]
         print(f"Arguments received: {len(args)}")
         for i in range(len(args)):
             print(f"Argument {i + 1}: {args[i]}")
-        print(f"{total} {len(arguments)}\n")
+        print(f"{total} {len(arguments)}")
 
 
 def main() -> None:
