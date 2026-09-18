9833b04d94e0ed6ae31e01b64f320cf483db63c3
Henrique Santana Frameschi <hesantan@c2r9p2.42sp.org.br>
2026-09-18 15:43:16 -0300

    ex1

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -3,12 +3,27 @@
 import sys
 
 
-def print_scores(scores: list[str]) -> None:
+def show_scores(scores: list[int]) -> None:
+    size = len(scores)
+    total = sum(scores)
+    average = round((total / size), 2)
+    higher = max(scores)
+    lower = min(scores)
+    print(f"Scores processed: {scores}")
+    print(f"Total players: {size}")
+    print(f"Total score: {total}")
+    print(f"Average score: {average}")
+    print(f"High score: {higher}")
+    print(f"Low score: {lower}")
+    print(f"Score range: {higher - lower}")
+
+
+def filter_scores(scores: list[str]) -> None:
     print("=== Player Score Analytics ===")
     str_scores = []
     num_scores = []
     err_msg = ("No scores provided. Usage: python3 ft_score_analytics.py "
-                "<score1> <score2> ...")
+               "<score1> <score2> ...")
     if len(scores) == 1:
         print(err_msg)
         return
@@ -16,33 +31,17 @@ def print_scores(scores: list[str]) -> None:
         try:
             num_scores.append(int(scores[i]))
         except ValueError:
-            x = scores.pop(i)
-            str_scores.append(x)
-    size = len(scores)
-    total = sum(scores)
-    average = round((total / size), 2)
-    higher = max(scores)
-    lower = min(scores)
-    print("Scores processed: [", end="")
-    for i in range(size):
-        print(scores[i], end="")
-        if i < (size - 1):
-            print(", ", end="")
-        elif i == (size - 1):
-            print("]")
-    print(f"Total players: {size}")
-    print(f"Total score: {total}")
-    print(f"Average score: {average}")
-    print(f"High score: {higher}")
-    print(f"Low score: {lower}")
-    print(f"Score range: {higher - lower}")
-    for i in range(1, len(str_scores)):
-        print(f"Invalid parameter: '{str_scores[i]}'")
-    print(err_msg)
+            str_scores.append(scores[i])
+    if (len(num_scores) == 0):
+        for i in range(1, len(str_scores)):
+            print(f"Invalid parameter: '{str_scores[i]}'")
+        print(err_msg)
+        return
+    show_scores(num_scores)
 
 
 def main() -> None:
-    print_scores(sys.argv)
+    filter_scores(sys.argv)
 
 
 if __name__ == "__main__":
