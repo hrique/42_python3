7cf278153ad0a2556cb6e32f18ad39c105042dc2
Henrique Santana Frameschi <hesantan@c3r4p4.42sp.org.br>
2026-09-22 18:26:03 -0300

    ex2

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -4,8 +4,21 @@ import math
 
 
 def get_player_pos() -> None:
-    ...
+    numbers = []
+    i = 0
+    while (i < 3):
+        try:
+            num = input("Enter new coordinates as floats in format 'x,y,z': ")
+            float(num)
+            numbers.append(num)
+            i += 1
+        except ValueError:
+            print("Invalid syntax")
+            continue
+    coord = tuple(coord)
+    print(f"Got a first tuple: {coord}")
+    print(f"It includes: X={coord[0]}, Y={coord[1]}, Z={coord[2]}")
 
 
 if __name__ == "__main__":
-    main()
+    get_player_pos()
