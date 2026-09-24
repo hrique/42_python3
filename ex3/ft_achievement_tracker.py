0320ca61fb9e126bc80b1abd405ad720b7cf3b62
hrique <hrique.frameschi@gmail.com>
2026-09-23 22:29:26 -0300

    ex3

---
+++ ex3/ft_achievement_tracker.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import random
+
+
+def gen_player_achievements():
+    achievements = {'Crafting Genius', 'Strategist', 'World Savior', 
+                    'Speed Runner', 'Survivor', 'Master Explorer', 
+                    'Treasure Hunter', 'Unstoppable', 'First Steps', 
+                    'Collector Supreme', 'Untouchable', 'Sharp Mind', 
+                    'Boss Slayer', 'Hidden Path Finder'}
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    main()
